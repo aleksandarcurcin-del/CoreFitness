c335b09a928e79970294c6eedf1163d5c4323d07
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-03-26 10:51:30 +0100

    Initial project setup with Clean Architecture structure

---
+++ CoreFitness2.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using CoreFitness2.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoreFitness2.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
