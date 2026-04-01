10f9e8dcdc2572b576d2d3d5575d122b01112905
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 10:11:41 +0200

    add GymClass Configuration to project

---
+++ CoreFitness2.Infrastructure/Configurations/GymClassEntityConfig.cs
@@ -0,0 +1,39 @@
+﻿using CoreFitness2.Domain.Entities.Classes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoreFitness2.Infrastructure.Configurations;
+
+public class GymClassEntityConfig : IEntityTypeConfiguration<GymClassEntity>
+{
+    public void Configure(EntityTypeBuilder<GymClassEntity> builder)
+    {
+        builder.ToTable("GymClasses");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(500);
+
+        builder.Property(x => x.Category)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Instructor)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.StartTime)
+            .IsRequired();
+
+        builder.Property(x => x.EndTime)
+            .IsRequired();
+
+        builder.Property(x => x.MaxParticipants)
+            .IsRequired();
+    }
+}
