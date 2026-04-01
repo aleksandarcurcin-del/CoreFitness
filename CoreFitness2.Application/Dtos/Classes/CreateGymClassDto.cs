be0ca08adb9d2ccf786d7670cca9689bab80d7c5
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 09:48:49 +0200

    add GymClass Dtos to project

---
+++ CoreFitness2.Application/Dtos/Classes/CreateGymClassDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CoreFitness2.Application.Dtos.Classes;
+
+public class CreateGymClassDto
+{
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public string Category { get; set; } = null!;
+    public string Instructor { get; set; } = null!;
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public int MaxParticipants { get; set; }
+
+}
---
+++ CoreFitness2.Application/Dtos/Classes/UpdateGymClassDto.cs
@@ -0,0 +1,17 @@
+﻿namespace CoreFitness2.Application.Dtos.Classes;
+
+public class UpdateGymClassDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+
+    public string Category { get; set; } = null!;
+    public string Instructor { get; set; } = null!;
+
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
+    public int MaxParticipants { get; set; }
+}
