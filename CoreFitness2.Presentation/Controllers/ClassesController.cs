e59802da2e3645d2148bae65bc0edc85ca6a6f45
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 12:16:57 +0200

    add ClassesController to project

---
+++ CoreFitness2.Presentation/Controllers/ClassesController.cs
@@ -0,0 +1,17 @@
+﻿using CoreFitness2.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness2.Presentation.Controllers;
+
+public class ClassesController(IGymClassService gymClassService) : Controller
+{
+
+    private readonly IGymClassService _gymClassService = gymClassService;
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var classes = await _gymClassService.GetAllAsync();
+        return View(classes);
+    }
+}
