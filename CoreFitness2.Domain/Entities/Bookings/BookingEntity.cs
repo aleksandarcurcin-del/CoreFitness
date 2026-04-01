6ebdce34cb29e5fbc0600e52e5c528050974ef6e
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 14:34:03 +0200

    add Booking entity to project

---
+++ CoreFitness2.Domain/Entities/Bookings/BookingEntity.cs
@@ -0,0 +1,15 @@
+﻿using CoreFitness2.Domain.Entities.Classes;
+
+namespace CoreFitness2.Domain.Entities.Bookings;
+
+public class BookingEntity
+{
+    public int Id { get; set; }
+
+    public string UserId { get; set; } = null!;
+
+    public int GymClassId { get; set; }
+    public GymClassEntity GymClass { get; set; } = null!;
+
+    public DateTime BookedAt { get; set; } = DateTime.UtcNow;
+}
