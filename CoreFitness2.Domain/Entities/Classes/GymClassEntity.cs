35df5e1a519b2deef19a69c76c52eff59252952c
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 09:39:09 +0200

    Add GymClass entity to project

---
+++ CoreFitness2.Domain/Entities/Classes/GymClassEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace CoreFitness2.Domain.Entities.Classes;
+
+public class GymClassEntity
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+
+    public string Category { get; set; } = null!;
+    public string Instructor { get; set; } = null!;
+
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
+    public int MaxParticipants { get; set; }
+
+    
+}
