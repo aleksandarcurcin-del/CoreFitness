165a40d83de5841e412cf69d611ed63269f6acc5
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 15:07:34 +0200

    add BookingRepository to project

---
+++ CoreFitness2.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,9 @@
+﻿using CoreFitness2.Application.Interfaces;
+using CoreFitness2.Domain.Entities.Bookings;
+using CoreFitness2.Infrastructure.Data;
+
+namespace CoreFitness2.Infrastructure.Repositories;
+
+public class BookingRepository(ApplicationDbContext context) : BaseRepository<BookingEntity>(context), IBookingRepository
+{
+}
