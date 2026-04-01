8ace846da66adae04147e440b17a00239d16e668
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 14:54:25 +0200

    add Booking Dtos

---
+++ CoreFitness2.Application/Dtos/Bookings/BookingDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreFitness2.Application.Dtos.Bookings;
+
+public class BookingDto
+{
+    public int Id { get; set; }
+    public int GymClassId { get; set; }
+    public string GymClassName { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public string Instructor { get; set; } = null!;
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public DateTime BookedAt { get; set; }
+}
---
+++ CoreFitness2.Application/Dtos/Bookings/CreateBookingDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreFitness2.Application.Dtos.Bookings;
+
+public class CreateBookingDto
+{
+    public string UserId { get; set; } = null!;
+    public int GymClassId { get; set; }
+}
