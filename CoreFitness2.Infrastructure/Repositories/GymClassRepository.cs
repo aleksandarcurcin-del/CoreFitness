b0c567287f6611231430dc94e855819867d57575
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 10:07:10 +0200

    add GymClass repository to project

---
+++ CoreFitness2.Infrastructure/Repositories/GymClassRepository.cs
@@ -0,0 +1,9 @@
+﻿using CoreFitness2.Application.Interfaces;
+using CoreFitness2.Domain.Entities.Classes;
+using CoreFitness2.Infrastructure.Data;
+
+namespace CoreFitness2.Infrastructure.Repositories;
+
+public class GymClassRepository(ApplicationDbContext context) : BaseRepository<GymClassEntity>(context), IGymClassRepository
+{
+}
