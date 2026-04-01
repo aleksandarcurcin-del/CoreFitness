1eab3caceebfacf847588ee1a46b7677db907534
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 12:56:59 +0200

    add GymClass ViewModels

---
+++ CoreFitness2.Presentation/ViewModels/Classes/GymClassCardViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CoreFitness2.Application.Dtos.Classes;
+
+namespace CoreFitness2.Presentation.ViewModels.GymClasses;
+
+public class GymClassCardViewModel
+{
+    public GymClassDto GymClass { get; set; } = null!;
+}
---
+++ CoreFitness2.Presentation/ViewModels/Classes/GymClassIndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CoreFitness2.Application.Dtos.Classes;
+
+namespace CoreFitness2.Presentation.ViewModels.Classes;
+
+public class GymClassIndexViewModel
+{
+    public IEnumerable<GymClassDto> Classes { get; set; } = Enumerable.Empty<GymClassDto>();
+}
