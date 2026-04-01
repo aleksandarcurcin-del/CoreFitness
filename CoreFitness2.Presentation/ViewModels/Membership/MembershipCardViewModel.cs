5f3b08f4fb7360f2ad52da47cf44d1e942fa6c84
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 07:56:42 +0200

    add membershipcard view model to update membership

---
+++ CoreFitness2.Presentation/ViewModels/Membership/MembershipCardViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CoreFitness2.Application.Dtos.Memberships;
+
+namespace CoreFitness2.Presentation.ViewModels.Membership;
+
+public class MembershipCardViewModel
+{
+    public MembershipPlanDto Plan { get; set; } = null!;
+
+    public UserMembershipDto? CurrentMembership { get; set; }
+}
