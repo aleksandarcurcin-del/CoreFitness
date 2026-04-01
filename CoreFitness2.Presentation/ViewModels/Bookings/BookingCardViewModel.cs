36279ee40ec7a0d5a0656077aa518361f74a4239
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-01 15:56:37 +0200

    add Booking viewmodels

---
+++ CoreFitness2.Presentation/ViewModels/Bookings/BookingCardViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CoreFitness2.Application.Dtos.Bookings;
+
+namespace CoreFitness2.Presentation.ViewModels.Bookings;
+
+public class BookingCardViewModel
+{
+    public BookingDto Booking { get; set; } = null!;
+}
---
+++ CoreFitness2.Presentation/ViewModels/Bookings/BookingIndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CoreFitness2.Application.Dtos.Bookings;
+
+namespace CoreFitness2.Presentation.ViewModels.Bookings;
+
+public class BookingIndexViewModel
+{
+    public IEnumerable<BookingDto> Bookings { get; set; } = Enumerable.Empty<BookingDto>();
+}
