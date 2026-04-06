1d10d18df43b0f44227a9942659e79602e463f67
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-06 11:20:12 +0200

    add ProfileDtos to project

---
+++ CoreFitness2.Application/Dtos/Profile/ProfileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreFitness2.Application.Dtos.Profile;
+
+public class ProfileDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string Email { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
+}
---
+++ CoreFitness2.Application/Dtos/Profile/UpdateProfileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreFitness2.Application.Dtos.Profile;
+
+public class UpdateProfileDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string Email { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
+}
