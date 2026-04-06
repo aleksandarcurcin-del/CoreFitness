91dcc19d760f2b5e5f6ac4690ab4e6b388c09fe3
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-06 11:39:08 +0200

    add Profile interface to project

---
+++ CoreFitness2.Application/Interfaces/IProfileService.cs
@@ -0,0 +1,11 @@
+﻿using CoreFitness2.Application.Dtos.Profile;
+using CoreFitness2.Application.Results;
+
+namespace CoreFitness2.Application.Interfaces;
+
+public interface IProfileService
+{
+    Task<ProfileDto?> GetProfileAsync(string userId);
+    Task<ServiceResult> UpdateProfileAsync(string userId, UpdateProfileDto dto);
+    Task<ServiceResult> DeleteProfileAsync(string userId);
+}
