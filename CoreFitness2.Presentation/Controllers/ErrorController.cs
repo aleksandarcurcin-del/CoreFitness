5f5b6a8fc29b00e4aeeffd08b59817258a660487
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-06 10:41:09 +0200

    add ErrorController

---
+++ CoreFitness2.Presentation/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness2.Presentation.Controllers;
+
+public class ErrorController : Controller
+{
+    [Route("Error/{statusCode}")]
+    public IActionResult NotFound(int statusCode)
+    {
+        return statusCode switch
+        {
+            404 => View("NotFound"),
+            _ => View()
+        };
+    }
+}
