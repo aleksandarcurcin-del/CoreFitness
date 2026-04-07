40c6c8e1b1c80b60e137c8a286608e2baf3d5421
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-07 11:20:48 +0200

    add ? to profileDto

--- CoreFitness2.Application/Interfaces/IUserAccountGateway.cs
+++ CoreFitness2.Application/Interfaces/IUserAccountGateway.cs
@@ -5,7 +5,7 @@ namespace CoreFitness2.Application.Interfaces;
 
 public interface IUserAccountGateway
 {
-    Task<ProfileDto> GetProfileAsync(string UserId);
+    Task<ProfileDto?> GetProfileAsync(string UserId);
     Task<ServiceResult> UpdateProfileAsync(string UserId, UpdateProfileDto dto);
     Task<ServiceResult> DeleteProfileAsync(string UserId);
 }
