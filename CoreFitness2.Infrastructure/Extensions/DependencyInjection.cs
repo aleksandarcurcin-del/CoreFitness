e16c6118d5c43c3c8ea706ac3e4ce310549ed9a3
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-07 11:38:13 +0200

    add UserAccountGateway to DI

--- CoreFitness2.Infrastructure/Extensions/DependencyInjection.cs
+++ CoreFitness2.Infrastructure/Extensions/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using CoreFitness2.Infrastructure.Data;
 using CoreFitness2.Infrastructure.Identity;
 using CoreFitness2.Infrastructure.Repositories;
+using CoreFitness2.Infrastructure.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -13,24 +14,27 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddScoped<IBookingRepository, BookingRepository>();
-        services.AddScoped<IGymClassRepository, GymClassRepository>();
-        services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
-        services.AddScoped<IMembershipPlanRepository, MembershipPlanRepository>();
-        services.AddScoped<IMembershipRepository, MembershipRepository>();
+        
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
         services.AddIdentity<ApplicationUser, IdentityRole>(options =>
         {
-            options.Password.RequireDigit = false;
-            options.Password.RequireLowercase = false;
-            options.Password.RequireUppercase = false;
-            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = true;
             options.Password.RequiredLength = 8;
         })
         .AddEntityFrameworkStores<ApplicationDbContext>()
         .AddDefaultTokenProviders();
+
+        services.AddScoped<IUserAccountGateway, UserAccountGateway>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
+        services.AddScoped<IGymClassRepository, GymClassRepository>();
+        services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+        services.AddScoped<IMembershipPlanRepository, MembershipPlanRepository>();
+        services.AddScoped<IMembershipRepository, MembershipRepository>();
 
         return services;
     }
