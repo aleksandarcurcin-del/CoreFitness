22c80c51ba34922246becbcd6bc3ce3a70ad74d7
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-07 12:01:20 +0200

    implement RegularExpression to SetPassword

--- CoreFitness2.Presentation/ViewModels/Account/SetPasswordViewModel.cs
+++ CoreFitness2.Presentation/ViewModels/Account/SetPasswordViewModel.cs
@@ -11,7 +11,10 @@ public class SetPasswordViewModel
 
     [Required]
     [DataType(DataType.Password)]
-    [MinLength(8)]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [RegularExpression(
+        @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$",
+        ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
     public string Password { get; set; } = null!;
 
     [Required]
