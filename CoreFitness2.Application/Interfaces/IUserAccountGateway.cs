cdcdb37664f7216cf52f5a155db891f733570e85
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-07 10:53:04 +0200

    add UserAccountGateway interface to create ProfileService in application

---
+++ CoreFitness2.Application/Interfaces/IUserAccountGateway.cs
@@ -0,0 +1,11 @@
+﻿using CoreFitness2.Application.Dtos.Profile;
+using CoreFitness2.Application.Results;
+
+namespace CoreFitness2.Application.Interfaces;
+
+public interface IUserAccountGateway
+{
+    Task<ProfileDto> GetProfileAsync(string UserId);
+    Task<ServiceResult> UpdateProfileAsync(string UserId, UpdateProfileDto dto);
+    Task<ServiceResult> DeleteProfileAsync(string UserId);
+}
