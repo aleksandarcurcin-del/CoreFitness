db7a82619f102bea5dc2c085b7bb5be28a53d0bd
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-07 11:01:30 +0200

    add ProfileService to Application

---
+++ CoreFitness2.Application/Services/ProfileService.cs
@@ -0,0 +1,46 @@
+﻿using CoreFitness2.Application.Dtos.Profile;
+using CoreFitness2.Application.Interfaces;
+using CoreFitness2.Application.Results;
+
+namespace CoreFitness2.Application.Services;
+
+public class ProfileService : IProfileService
+{
+    private readonly IUserAccountGateway _userAccountGateway;
+    public ProfileService(IUserAccountGateway userAccountGateway)
+    {
+        _userAccountGateway = userAccountGateway;
+    }
+
+
+    public async Task<ProfileDto?> GetProfileAsync(string userId)
+    {
+        if (string.IsNullOrWhiteSpace(userId))
+            return null;
+
+        return await _userAccountGateway.GetProfileAsync(userId);
+    }
+
+    public async Task<ServiceResult> UpdateProfileAsync(string userId, UpdateProfileDto dto)
+    {
+        if (!string.IsNullOrWhiteSpace(userId))
+            return ServiceResult.Failure("User Was not found");
+
+        if (dto == null)
+            return ServiceResult.Failure("Invalid profile data");
+
+        return await _userAccountGateway.UpdateProfileAsync(userId, dto);
+    }
+
+    public async Task<ServiceResult> DeleteProfileAsync(string userId)
+    {
+        if (string.IsNullOrWhiteSpace(userId))
+            return ServiceResult.Failure("User Was not found");
+
+        return await _userAccountGateway.DeleteProfileAsync(userId);
+    }
+
+    
+
+    
+}
