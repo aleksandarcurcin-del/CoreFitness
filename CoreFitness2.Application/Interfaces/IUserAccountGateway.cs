125194ed379107256d9b2830d015cbf657262894
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-08 10:18:35 +0200

    add ApplicationUser to Gateway

--- CoreFitness2.Application/Interfaces/IUserAccountGateway.cs
+++ CoreFitness2.Application/Interfaces/IUserAccountGateway.cs
@@ -1,11 +1,8 @@
-﻿using CoreFitness2.Application.Dtos.Profile;
-using CoreFitness2.Application.Results;
+﻿using CoreFitness2.Application.Results;
 
 namespace CoreFitness2.Application.Interfaces;
 
 public interface IUserAccountGateway
 {
-    Task<ProfileDto?> GetProfileAsync(string UserId);
-    Task<ServiceResult> UpdateProfileAsync(string UserId, UpdateProfileDto dto);
-    Task<ServiceResult> DeleteProfileAsync(string UserId);
+    Task<ServiceResult> DeleteIdentityUserAsync(string applicationUserId);
 }
