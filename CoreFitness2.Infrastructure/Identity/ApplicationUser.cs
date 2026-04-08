56703c9fec6ecfd39465573ef473734b8a257a8d
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-08 09:47:21 +0200

    Remove firstname and lastname

--- CoreFitness2.Infrastructure/Identity/ApplicationUser.cs
+++ CoreFitness2.Infrastructure/Identity/ApplicationUser.cs
@@ -1,18 +1,7 @@
-﻿using CoreFitness2.Domain.Entities.Bookings;
-using CoreFitness2.Domain.Entities.MembershipPlans;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace CoreFitness2.Infrastructure.Identity;
 
 public class ApplicationUser : IdentityUser
 {
-    
-    public string? FirstName { get; set; }
-
-    
-    public string? LastName { get; set; }
-
-    public MembershipEntity? Membership { get; set; }
-
-    public ICollection<BookingEntity> Bookings { get; set; } = [];
 }
