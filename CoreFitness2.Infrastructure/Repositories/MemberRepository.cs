b16b3087711600eec7044137772b470dca5dda8c
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-08 10:23:52 +0200

    add Member Repository to project

---
+++ CoreFitness2.Infrastructure/Repositories/MemberRepository.cs
@@ -0,0 +1,9 @@
+﻿using CoreFitness2.Application.Interfaces;
+using CoreFitness2.Domain.Entities.Members;
+using CoreFitness2.Infrastructure.Data;
+
+namespace CoreFitness2.Infrastructure.Repositories;
+
+public class MemberRepository(ApplicationDbContext Context) : BaseRepository<MemberEntity>(Context), IMemberRepository
+{
+}
