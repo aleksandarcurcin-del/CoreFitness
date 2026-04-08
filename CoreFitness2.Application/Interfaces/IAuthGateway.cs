8ba11c739e820eb2f754410a96ce5927c0217db8
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-08 11:34:25 +0200

    add IAuthgateway to project

---
+++ CoreFitness2.Application/Interfaces/IAuthGateway.cs
@@ -0,0 +1,11 @@
+﻿using CoreFitness2.Application.Dtos.Auth;
+using CoreFitness2.Application.Results;
+
+namespace CoreFitness2.Application.Interfaces;
+
+public interface IAuthGateway
+{
+    Task<(ServiceResult Result, string? ApplicationUserId)> RegisterIdentityUserAsync(SignUpDto dto);
+    Task<ServiceResult> SignInAsync(SignInDto dto);
+    Task SignOutAsync();
+}
