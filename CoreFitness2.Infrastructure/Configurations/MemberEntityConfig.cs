9804b40e3b45818d92879b6c35f1294f26bce6ce
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-08 09:45:07 +0200

    add MemberEntity Configuration to project

---
+++ CoreFitness2.Infrastructure/Configurations/MemberEntityConfig.cs
@@ -0,0 +1,37 @@
+﻿using CoreFitness2.Domain.Entities.Members;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoreFitness2.Infrastructure.Configurations;
+
+public class MemberEntityConfig : IEntityTypeConfiguration<MemberEntity>
+{
+    public void Configure(EntityTypeBuilder<MemberEntity> builder)
+    {
+        builder.ToTable("Members");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.ApplicationUserId)
+            .IsRequired();
+
+        builder.Property(x => x.FirstName)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.LastName)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        builder.Property(x => x.PhoneNumber)
+            .HasMaxLength(50);
+
+        builder.Property(x => x.ProfileImageUrl)
+            .HasMaxLength(500);
+
+        builder.HasIndex(x => x.ApplicationUserId)
+            .IsUnique();
+    }
+}
