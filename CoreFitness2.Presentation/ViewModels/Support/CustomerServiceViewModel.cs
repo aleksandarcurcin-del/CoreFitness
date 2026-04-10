d38d61a9e125138f7f7ea4013dcdf1616ebb3788
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-10 12:41:33 +0200

    add CustomerService ViewModel

---
+++ CoreFitness2.Presentation/ViewModels/Support/CustomerServiceViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreFitness2.Presentation.ViewModels.Support
+{
+    public class CustomerServiceViewModel
+    {
+
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address.")]
+        public string Email { get; set; } = null!;
+
+
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Message is required.")]
+        public string Message { get; set; } = null!;
+
+
+    }
+}
