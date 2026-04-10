eb8e5ad0b411f16d5b2f1908139613186598c781
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-10 12:43:51 +0200

    add SupportController

---
+++ CoreFitness2.Presentation/Controllers/SupportController.cs
@@ -0,0 +1,27 @@
+﻿using CoreFitness2.Presentation.ViewModels.Support;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness2.Presentation.Controllers;
+
+public class SupportController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new CustomerServiceViewModel());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Index(CustomerServiceViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            TempData["ErrorMessage"] = "Please correct the form and try again.";
+            return View(model);
+        }
+
+        TempData["SuccessMessage"] = "Your message has been sent successfully.";
+        return RedirectToAction(nameof(Index));
+    }
+}
