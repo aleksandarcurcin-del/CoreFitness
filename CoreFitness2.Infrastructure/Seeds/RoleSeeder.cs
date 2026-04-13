074e3f2ee3fd37634c3eb18ecc44f7195be574e4
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-13 09:22:14 +0200

    Add RoleSeeder to project

---
+++ CoreFitness2.Infrastructure/Seeds/RoleSeeder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CoreFitness2.Infrastructure.Seeds;
+
+public class RoleSeeder
+{
+    public static async Task SeedAsync(RoleManager<IdentityRole> roleManager)
+    {
+        var roles = new[] { "Admin", "Member" };
+        foreach (var role in roles)
+        {
+            var exists = await roleManager.RoleExistsAsync(role);
+            if (!exists)
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+}
