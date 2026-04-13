5d119c659cac6582ab9452159a1348b781b0f3b2
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-13 09:44:14 +0200

    add AdminSeeder to infrastructure

---
+++ CoreFitness2.Infrastructure/Seeds/AdminSeeder.cs
@@ -0,0 +1,29 @@
+﻿using CoreFitness2.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace CoreFitness2.Infrastructure.Seeds;
+
+public class AdminSeeder
+{
+    public static async Task SeedAdminAsync(
+    UserManager<ApplicationUser> userManager,
+    RoleManager<IdentityRole> roleManager)
+    {
+        var email = "admin@corefitness.com";
+        var password = "Admin123!";
+
+        var user = await userManager.FindByEmailAsync(email);
+
+        if (user == null)
+        {
+            user = new ApplicationUser
+            {
+                UserName = email,
+                Email = email
+            };
+
+            await userManager.CreateAsync(user, password);
+            await userManager.AddToRoleAsync(user, "Admin");
+        }
+    }
+}
