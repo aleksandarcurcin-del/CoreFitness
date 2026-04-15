6d9f438d06d43af39775ba0faee8e8219a0c45d5
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-04-15 09:57:27 +0200

    add TrainingController for PersonalTraining, grouptraining and padel pages

---
+++ CoreFitness2.Presentation/Controllers/TrainingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness2.Presentation.Controllers;
+
+public class TrainingController : Controller
+{
+    public IActionResult PersonalTraining()
+    {
+        return View();
+    }
+
+    public IActionResult GroupTraining()
+    {
+        return View();
+    }
+
+    public IActionResult Padel()
+    {
+        return View();
+    }
+}
